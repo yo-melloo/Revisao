658a5fe5c41e924cf518f92f7b26816e70a7177c
Gustavo Mello <142965241+yo-melloo@users.noreply.github.com>
2025-10-04 21:11:00 +0000

    feat(api): adiciona campos e validação de nome, email e avatar

--- backend/src/main/java/com/mello/revisao/controllers/UserController.java
+++ backend/src/main/java/com/mello/revisao/controllers/UserController.java
@@ -14,12 +14,12 @@ public class UserController {
 
     private final UserService userService;
 
-    public UserController(UserService userService){
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @GetMapping
-    public List<UserModel> mostarUsuarios(){
+    public List<UserModel> mostarUsuarios() {
         return userService.mostrarUsuarios();
     }
 }
